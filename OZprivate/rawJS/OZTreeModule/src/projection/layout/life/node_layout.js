1b259b96e4f6217787ba50207c62f86ff9f1686c
Leila <leila@noxaka.com>
2017-10-12 00:42:31 +0200

    Réduit la dimension de l'image des groupes pour être bien entre les 2 textes

--- OZprivate/rawJS/OZTreeModule/src/projection/layout/life/node_layout.js
+++ OZprivate/rawJS/OZTreeModule/src/projection/layout/life/node_layout.js
@@ -22,7 +22,7 @@ class LifeNodeLayout extends NodeLayoutBase
 
     autopic_set_shapes(node, x, y, width, height, shapes) 
     {
-        let radiusr = 0.3 * width;
+        let radiusr = 0.2 * width;
 
         // get a random picture
         if (node.image_index === undefined)
@@ -48,8 +48,7 @@ class LifeNodeLayout extends NodeLayoutBase
         shapes.push(image_shape);
     }
 
-    interior_circle_shapes(node, shapes) {
-        
+    interior_circle_shapes(node, shapes) {   
         // uncomment this to get guidelines
         /*
         let horizonarc = ArcShape.create();
@@ -69,50 +68,6 @@ class LifeNodeLayout extends NodeLayoutBase
         horizonarc.stroke.color = color_theme.get_color('interior.circle.stroke', node);
         shapes.push(horizonarc);
         */
-
-        /* let condition = node.rvar > config.projection.node_low_res_thres && config.projection.interior_circle_draw;
-        if (!condition) return;
-        let arc_shape = ArcShape.create();
-        if (!this.hovered && this.live_area_interior_circle_test(node)) {
-            this.hovered = true;
-            this.hovering = true;
-            live_area_config.interior_low_res_circle.register_button_event(node);
-        }
-        arc_shape.x = node.xvar + node.rvar * node.arcx;
-        arc_shape.y = node.yvar + node.rvar * node.arcy;
-        //arc_shape.r = node.rvar * node.arcr * (1-config.projection.partl2/2.0);
-        arc_shape.r = 0.5 * config.projection.Twidth * (config.projection.partc - config.projection.partl2) * node.rvar;
-        arc_shape.circle = true;
-        arc_shape.height = 2;
-        arc_shape.order = "fill_first";
-        arc_shape.do_stroke = true;
-        arc_shape.stroke.line_width = node.arcr * config.projection.partl2 * node.rvar;
-        arc_shape.stroke.color = color_theme.get_color('interior.circle.stroke', node);*/
-        /*
-        // James - I think this code allows the stroke around an interior circle to be a different colour depending on whether it's a search hit or not. This never happens, which I think is beacuse it's bugged
-        // for now I'm simply removing as this was not such an important feature anyway.
-        if (node.has_child) {
-            let is_common_ancestor = true;
-            let length = node.children.length;
-            for (let i=0; i<length; i++) {
-                if (!node.children[i].route_to_search) { // the bug is here, requires all descendents to be marked in order for it to work
-                    is_common_ancestor = false;
-                    break;
-                }
-            }
-            if (is_common_ancestor) {
-                arc_shape.stroke.color = color_theme.get_color('interior.circle_searchin.stroke', node)
-            }
-        }
-        */
-        /*arc_shape.do_fill = true;
-        arc_shape.fill.color = color_theme.get_color('interior.circle.fill', node);
-        if (this.hovering) {
-            arc_shape.stroke.color = color_theme.get_color('interior.circle_hover.stroke', node);
-            arc_shape.fill.color = color_theme.get_color('interior.circle_hover.fill', node);
-        }
-        this.hovering = false;
-        shapes.push(arc_shape);*/
     }
 }
 
